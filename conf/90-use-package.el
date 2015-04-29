32a6cbb2c5e986f4a87e395e793d7d971589d6cb
YukiWatanabe <f2b239@gmail.com>
2015-04-29 15:46:54 +0900

    少量追加

---
+++ conf/90-use-package.el
@@ -0,0 +1,60 @@
+(require 'use-package)
+(require 'bind-key)
+
+(use-package shell-pop
+  :commands (shell-pop)
+  :bind (("C-p" . shell-pop))
+  :config
+  (use-package exec-path-from-shell
+	:config
+	(exec-path-from-shell-initialize)
+	)
+
+  (add-hook 'eshell-mode-hook
+		  '(lambda()
+			 (progn
+			   (define-key eshell-mode-map (kbd "C-f") 'find-file-other-window)
+			   ;;(define-key eshell-mode-map (kbd "<left>"))
+			   )))
+
+  )
+
+(use-package magit
+  :commands (magit-status)
+  :config
+  ;;magit Congig
+  )
+
+(use-package howm
+  :commands (howm-menu)
+  :config
+  ;;howm Config
+
+  (setq howm-menu-lang 'ja)
+  
+  ;; howm directoryの場所
+  (setq howm-directory "~/Dropbox/emacs/howm/")
+  
+  (global-set-key "\C-c,," 'howm-menu)
+  (mapc
+   (lambda (f)
+	 (autoload f
+	   "howm" "Hitori Otegaru Wiki Modoki" t))
+   '(howm-menu howm-list-all howm-list-recent
+			   howm-list-grep howm-create
+			   howm-keyword-to-kill-ring))
+  
+  (defun howm-save-buffer-and-kill ()
+	"howmメモを保存と同時に閉じます。"
+	(interactive)
+	(when (and (buffer-file-name)
+			   (string-match "\\.txt" (buffer-file-name)))
+	  (save-buffer)
+	  (kill-buffer nil)))
+  
+  ;; C-c C-cでメモの保存と同時にバッファを閉じる
+  (define-key howm-mode-map (kbd "C-c C-c") 'howm-save-buffer-and-kill)
+  
+  )
+
+
